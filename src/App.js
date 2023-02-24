47bde4fc6553ac373852efb52ede93302828463d
Bishal Bashyal <waglemanish97@gmail.com>
2023-02-24 13:37:01 +0545

    Initial Sidebar Setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Sidebar from './components/sidebar';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sidebar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/sidebar.js
@@ -0,0 +1,27 @@
+function Sidebar(){
+
+    return(
+        <div className="flex w-[124px] h-screen bg-slate-500 text-white">
+        <ul className=" my-8 flex flex-grow flex-col w-full">
+            <li  className=" hover:bg-slate-600 w-full">
+                Server 1
+            </li>
+            <li className=" hover:bg-slate-600 w-full">
+                Server 1
+            </li>
+            <li className=" hover:bg-slate-600 w-full">
+                Server 1
+            </li>
+            <li className=" hover:bg-slate-600 w-full">
+                Server 1
+            </li>
+            <li className=" hover:bg-slate-600 w-full">
+                Server 1
+            </li>
+        </ul>
+        </div>
+
+    );
+}
+
+export default Sidebar;
