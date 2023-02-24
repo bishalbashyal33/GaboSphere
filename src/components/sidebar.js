228dfe78b4f23960ab8bc17af05e22a24ae90421
Bishal Bashyal <waglemanish97@gmail.com>
2023-02-24 15:35:05 +0545

    Sidebar Initial Wireframe Completed for GaboSphere

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,22 +1,172 @@
 function Sidebar(){
 
     return(
-        <div className="flex w-[124px] h-screen bg-slate-500 text-white">
-        <ul className=" my-8 flex flex-grow flex-col w-full">
-            <li  className=" hover:bg-slate-600 w-full">
-                Server 1
+        <div className="absolute top-0 left-0 flex overflow-scroll scrollbar-hide w-[48px] h-screen bg-slate-500 text-white">
+        <ul className=" flex flex-grow flex-col w-full">
+        <li  className=" hover:bg-slate-600 w-full h-[48px] px-2 ">
+                <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://neural.love/cdn/ai-photostock/1ed5a264-5a68-66e6-859c-89059355a2b9/2.jpg?Expires=1680307199&Signature=dm-l1bJ0K7LENjyWZic5evFw5zCxZLuRGDIGttHC2boqxIetLoZS7CA4lPENzFxd-tL3FJku5vyX~ZSbAJOOEaxRgHf~tNA4aBE8HNZZVmvnORxmT6QzZWPiP1jLbYZ0TWXP--aEqGqJKhC1Ue6Q5h8WuihaPE5t4UbGowHK7yYzSWccEZo67VDn3bdMECLvBmQ63FS-bQRL~N~njbtWx95AY-mNaqp6j39ZrMZDN1yaZAkjvbzL1c394O1NYS06pNueSjtgJK21-oG0Gx0YN0voMns0mNwpB08EGONzqx2h~lXAqaYNK7slQqTSHCcmvNRS8AwDv~GAKE3gzQNehg__&Key-Pair-Id=K2RFTOXRBNSROX"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
             </li>
-            <li className=" hover:bg-slate-600 w-full">
-                Server 1
+            <div class="px-2">
+            <div class="border-t-4 border-gray-300 rounded-xl w-8 px-2"></div>
+            </div>
+            <li  className=" hover:bg-slate-600 w-full h-[48px] px-2 flex">
+                <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
             </li>
-            <li className=" hover:bg-slate-600 w-full">
-                Server 1
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex ">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
             </li>
-            <li className=" hover:bg-slate-600 w-full">
-                Server 1
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
             </li>
-            <li className=" hover:bg-slate-600 w-full">
-                Server 1
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
+            </li>
+            <li className=" hover:bg-slate-600 px-2 w-full h-[48px] flex">
+            <div className="relative py-2 w-full" >
+                <img className="w-8 h-8 rounded-xl " src="https://www.pngitem.com/pimgs/m/664-6644509_icon-react-js-logo-hd-png-download.png"></img>
+                <div className="-mx-2 absolute inset-0 bg-black opacity-0 hover:opacity-20 transition duration-300"></div>
+                </div>
             </li>
         </ul>
         </div>
